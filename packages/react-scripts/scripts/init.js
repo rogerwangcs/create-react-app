c2e0373f089bb38015923c9c4050124d73f1e316
Roger Wang <rogerwangcs@gmail.com>
2020-01-04 22:38:01 -0800

    testing modifications

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -75,6 +75,12 @@ function tryGitInit(appPath) {
   }
 }
 
+// rogerwangcs-react-scripts start
+function rogerwangcsLog() {
+  console.log(chalk.blueBright('Custom create-react-app for rogerwangcs'));
+}
+// rogerwangcs-react-scripts end
+
 module.exports = function(
   appPath,
   appName,
@@ -285,6 +291,7 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  rogerwangcsLog();
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
